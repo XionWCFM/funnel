f12233061ec89385692991d80c23bbc624f4c033
devgiljong <calf-love@naver.com>
2024-08-11 02:47:52 +0900

    :construction: Funnel client format

--- packages/funnel-client/src/external/funnel-client.ts
+++ packages/funnel-client/src/external/funnel-client.ts
@@ -21,7 +21,6 @@ export class FunnelClient<T extends NonEmptyArray<string>> {
     this.deleteStep = this.deleteStep.bind(this);
     this.stringifyStep = this.stringifyStep.bind(this);
     this.parseQueryString = this.parseQueryString.bind(this);
-
   }
 
   createStep(step: T[number], options?: CreateStepOptionsType) {
