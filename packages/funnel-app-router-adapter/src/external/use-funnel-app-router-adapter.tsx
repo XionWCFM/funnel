0db8a3c6d28ac804d0afdfcf06db9855001ca843
devgiljong <calf-love@naver.com>
2024-08-14 18:01:32 +0900

    :bookmark: 0.0.4 -> 0.0.5 | minify add

--- packages/funnel-app-router-adapter/src/external/use-funnel-app-router-adapter.tsx
+++ packages/funnel-app-router-adapter/src/external/use-funnel-app-router-adapter.tsx
@@ -5,11 +5,9 @@ import { useSearchParams } from "next/navigation";
 export const useFunnelAppRouterAdapter = <Steps extends NonEmptyArray<string>>(
   options: Omit<FunnelOptions<Steps>, "step">,
 ) => {
-  const funnelId = options.funnelId;
   const searchParams = useSearchParams();
-  const queryStep = searchParams.get(funnelId);
   const funnelClient = new FunnelClient(options);
-  const step = (queryStep ?? undefined) as Steps[number] | undefined;
+  const step = (searchParams.get(options.funnelId) ?? undefined) as Steps[number] | undefined;
   const [Funnel, { onStepChange, ...controller }] = useCoreFunnel({ ...options, step });
   return [Funnel, { ...controller, createStep: funnelClient.createStep }] as const;
 };
